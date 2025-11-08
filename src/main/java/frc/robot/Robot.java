f12ac2f6c9acc7f3b3b61df73e3563da7418dc27
aras arkan <83475962+imawss@users.noreply.github.com>
2025-11-09 02:07:36 +0300

    simgui test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,13 +6,10 @@ package frc.robot;
 
 import org.ironmaple.simulation.SimulatedArena;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.constants.RobotConstants;
-import frc.robot.utils.Logger;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
